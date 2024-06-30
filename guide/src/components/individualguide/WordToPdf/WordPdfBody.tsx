a653851651499a079e8141cba640862ceec2f596
Rasmus <rasmustofthermansen@gmail.com>
2024-06-30 03:56:18 +0200

    Fixed a picture and changes to css to make it look better

--- guide/src/components/individualguide/WordToPdf/WordPdfBody.tsx
+++ guide/src/components/individualguide/WordToPdf/WordPdfBody.tsx
@@ -7,10 +7,13 @@ import picture5 from '../../../assets/WordToPdfPictures/picture5.png'
 import picture6 from '../../../assets/WordToPdfPictures/picture6.png'
 function WordPdfBody() {
   return (
-    <div className='flex flex-col items-left w-full'>
+    <div className='flex flex-col items-left w-full font-serif'>
       <div className='w-full h-full'>
-        <h1 className='text-center font-semibold text-3xl font-Inter'>Velkommen til Word til PDF</h1>
-        <div className='pt-6 pl-2 space-y-10 text-xs'>
+        <div className='text-center font-semibold'>
+        <h1 className='text-3xl'>Velkommen til Word til PDF</h1>
+        <h1 className=' pt-4'>Du vil lærer hvordan man konvertere fra word til pdf</h1>
+        </div>
+        <div className='pt-16 pl-2 space-y-10 text-xs'>
           <h3 className="font-normal text-lg">
             <span className="font-bold underline text-xl">1.</span> Find det Word-dokument, du vil lave om til en PDF-fil, og åben det ved at dobbeltklikke på det.
           </h3>
@@ -19,12 +22,12 @@ function WordPdfBody() {
           <h3 className="font-normal text-lg">
             <span className="font-bold underline text-xl">2.</span> Gå til 'Filer'-menuen: I øverste venstre hjørne af Word-vinduet, klik på 'Filer'. Denne knap kan også se ud som et lille ikon eller have ordet 'File' afhængigt af din version af Word.
           </h3>
-          <img src={picture2} alt="Fail" className='w-1/3'/>
+          <img src={picture2} alt="Fail" className='w-1/2'/>
 
           <h3 className="font-normal text-lg">
             <span className="font-bold underline text-xl">3.</span> Vælg 'Gem som': Når du er i 'Filer'-menuen, vælg 'Gem som'
           </h3>
-          <img src={picture3} alt="Fail" className='h-64'/>
+          <img src={picture3} alt="Fail" className='h-96'/>
 
           <h3 className="font-normal text-lg">
             <span className="font-bold underline text-xl">4.</span> Vælg hvor du vil gemme filen: Vælg den placering på din computer, hvor du vil gemme din nye PDF-fil. Det kan være på skrivebordet, i en bestemt mappe.
@@ -39,7 +42,7 @@ function WordPdfBody() {
           <h3 className="font-normal text-lg">
             <span className="font-bold underline text-xl">6.</span> Gem filen: Efter du har valgt PDF-formatet, klik på 'Gem' knappen.
           </h3>
-          <img src={picture6} alt="Fail" className='h-64 w-1/2'/>
+          <img src={picture6} alt="Fail" className='w-1/2'/>
 
         </div>
       </div>
